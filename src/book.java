d31c9c308b2ac28a22b75a1ae260e36b8844fb56
FattyRatty <60388760+emilasbringer@users.noreply.github.com>
2021-09-17 08:30:41 +0200

    y

---
+++ src/book.java
@@ -0,0 +1,11 @@
+public class book {
+    private String title;
+    private Author author;
+
+    public void Book(String title, Author author) {
+        this.title = title;
+        this.author = author;
+
+
+    }
+}
